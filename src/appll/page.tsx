2af3dee201ecafd8cf77b9acf40a57e1acb5f3cb
Nathan Tchinda <sadenachbinathan@gmail.com>
2024-12-06 05:11:00 +0100

    add meta data

--- src/appll/page.tsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function Home() {
-
-
-  return <div className="bg-gray-50 min-h-screen flex flex-col">
-
-  </div>
-}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import NavBar from '@/components/Layout/NavBar';
 import type { AppProps } from 'next/app';
 import "@/styles/globals.css";
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: "Genuka Technical Test",
+    description: "Genuka Technical Test",
+};
 const MyApp = ({ Component, pageProps }: AppProps) => {
     return (
         <div className="bg-gray-50 min-h-screen flex flex-col">
